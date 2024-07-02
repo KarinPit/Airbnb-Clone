3c6927d7c0d5e096fcd7953be0c543d2db0f6fe7
KarinPit <99114944+KarinPit@users.noreply.github.com>
2024-07-02 22:50:35 +0300

    finished header responsiveness, no scroll effect yet

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export function App() {
 
                     <main className='container'>
                         <Routes>
-                            <Route path="/" element={<Home />} />
+                            {/* <Route path="/" element={<Home />} /> */}
                         </Routes>
                     </main>
 
