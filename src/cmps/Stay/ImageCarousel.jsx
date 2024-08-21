12cd41a1058696b8458ff42dfe33e9f75b60ab68
Karin Pitlik <karinpitlik@Karins-MacBook-Pro.local>
2024-08-21 17:31:44 +0300

    added most of the carousel logic. still working on carousel dots logic

--- src/cmps/Stay/ImageCarousel.jsx
+++ src/cmps/Stay/ImageCarousel.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+
 
 export function ImageCarousel({ stayImages, isGuestFavorite, stayId }) {
     const [currentIndexes, setCurrentIndexes] = useState({ [stayId]: 0 })
     const [isHovered, setIsHovered] = useState(null)
+    const [slideDirection, setSlideDirection] = useState('');
+    const totalImages = stayImages.additional.length;
+    const dotTransform = currentIndexes[stayId] > 2 && currentIndexes[stayId] < totalImages - 2
+        ? `translateX(-18px)`
+        : 'translateX(0)';
+
 
     useEffect(() => {
         setCurrentIndexes({ [stayId]: 0 })
     }, [stayId])
 
     function handleArrowClick(direction) {
+        setSlideDirection(direction)
         setCurrentIndexes(prev => ({
             ...prev,
             [stayId]: direction === 'right'
@@ -17,10 +26,26 @@ export function ImageCarousel({ stayImages, isGuestFavorite, stayId }) {
         }))
     }
 
+    function getDotClass(index) {
+        const currentIndex = currentIndexes[stayId];
+
+        if (currentIndex < 3) {
+            return currentIndex === index ? 'active-dot' : '';
+        } else if (currentIndex >= 3 && currentIndex <= totalImages - 3) {
+            return index === 2 ? 'active-dot' : '';
+        } else {
+            return currentIndex === index + (totalImages - 3) ? 'active-dot' : '';
+        }
+    }
+
     return (
         <div className="stay-carousel"
-            onMouseEnter={() => setIsHovered(stayId)}
-            onMouseLeave={() => setIsHovered(null)}>
+            onMouseEnter={() =>
+                setIsHovered(stayId)
+            }
+            onMouseLeave={() =>
+                setIsHovered(null)
+            }>
 
             <div className="add-to-favorites">
                 {isGuestFavorite &&
@@ -35,27 +60,51 @@ export function ImageCarousel({ stayImages, isGuestFavorite, stayId }) {
                 </div>
             </div>
 
-            <div className={`arrows ${isHovered === stayId ? 'show' : ''}`}>
-                <div className={currentIndexes[stayId] === 0 ? 'hide' : ''}
-                    onClick={() => handleArrowClick('left')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="left-arrow">
-                        <path d="M8.5 12.8l5.7 5.6c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4l-4.9-5 4.9-5c.4-.4.4-1 0-1.4-.2-.2-.4-.3-.7-.3-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6z"></path>
-                    </svg>
-                </div>
+            <AnimatePresence>
+                {isHovered === stayId &&
+                    <motion.div className="arrows"
+                        key="arrows"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{
+                            opacity: { duration: 0.1 },
 
-                <div className={currentIndexes[stayId] === stayImages.additional.length - 1 ? 'hide' : ''}
-                    onClick={() => handleArrowClick('right')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="right-arrow">
-                        <path d="M15.54 11.29L9.88 5.64a1 1 0 00-1.42 0 1 1 0 000 1.41l4.95 5L8.46 17a1 1 0 000 1.41 1 1 0 00.71.3 1 1 0 00.71-.3l5.66-5.65a1 1 0 000-1.41z"></path>
-                    </svg>
-                </div>
+                        }}
+                    >
+                        <div className={currentIndexes[stayId] === 0 ? 'hide' : ''}
+                            onClick={() => handleArrowClick('left')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="left-arrow">
+                                <path d="M8.5 12.8l5.7 5.6c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4l-4.9-5 4.9-5c.4-.4.4-1 0-1.4-.2-.2-.4-.3-.7-.3-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6z"></path>
+                            </svg>
+                        </div>
+
+                        <div className={currentIndexes[stayId] === stayImages.additional.length - 1 ? 'hide' : ''}
+                            onClick={() => handleArrowClick('right')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="right-arrow">
+                                <path d="M15.54 11.29L9.88 5.64a1 1 0 00-1.42 0 1 1 0 000 1.41l4.95 5L8.46 17a1 1 0 000 1.41 1 1 0 00.71.3 1 1 0 00.71-.3l5.66-5.65a1 1 0 000-1.41z"></path>
+                            </svg>
+                        </div>
+                    </motion.div>}
+            </AnimatePresence>
+
+
+            <div className={`carousel ${slideDirection}`} style={{ transform: `translateX(-${currentIndexes[stayId] * 100}%)` }}>
+                <img src={stayImages.picture_url} alt="Stay Image"></img>
+                {stayImages.additional.map(image => {
+                    return (
+                        <img key={image} src={image} alt="Stay Image"></img>
+                    )
+                })}
             </div>
 
-            <div>
-                <img src={currentIndexes[stayId] === 0
-                    ? stayImages.picture_url
-                    : stayImages.additional[currentIndexes[stayId]]}
-                    alt="Stay Image" />
+            <div className="carousel-dots">
+                <div className="dots-container" style={{ transform: dotTransform }}>
+                    {Array.from({ length: 3 * stayImages.additional.length }).map((_, index) => (
+                        <div key={index} className={`${getDotClass(index)}`}>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
